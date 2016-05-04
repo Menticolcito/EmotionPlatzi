eb01920437c7e8f1b12db86b8cb3e235b65bf6b5
Rubén Melo <rbmcar2@gmail.com>
2016-05-04 11:38:26 -0500

    Add project files.

---
+++ EmotionPlatzi/Models/Home.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionPlatzi.Models
+{
+    public class Home
+    {
+        public int Id { get; set; }
+        public string WelcomeMessage { get; set; }
+        public string FooterMessage { get; set; } = "Footer by Rubén Melo";
+    }
+}
